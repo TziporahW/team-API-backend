b641cdebd49e5da8b3868e68f4884bcdae64c01b
Wendy <69648973+thewendels@users.noreply.github.com>
2020-12-28 11:44:32 -0600

    adds update to students routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/students" => "students#index"
-    get "/students/:id" => "students#show"
     post "/students" => "students#create"
+    get "/students/:id" => "students#show"
+    patch "/students/:id" => "students#update"
   end
 end
