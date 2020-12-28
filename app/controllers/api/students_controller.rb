52333d3c2f4f611964253d3b19160063d68e4752
Wendy <69648973+thewendels@users.noreply.github.com>
2020-12-28 13:58:31 -0600

    adds model validations and students create action

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -1,10 +1,23 @@
 class Api::StudentsController < ApplicationController
   def create
     student = Student.new(
-      # name: params[:name],
+      first_name: params[:first_name]
+      last_name: params[:last_name],
       email: params[:email],
       password: params[:password],
       password_confirmation: params[:password_confirmation],
+      phone_number: params[:phone_number],
+      short_bio: params[:short_bio],
+      linkedin_url: params[:linkedin_url],
+      twitter_handle: params[:twitter_handle],
+      personal_website_url: params
+      [:personal_website_url],
+      online_resume_url: params
+      [:online_resume_url],
+      github_url: params
+      [:github_url],
+      photo_url: params
+      [:photo_url]
     )
     if student.save
       render json: { message: "student created successfully" }, status: :created
