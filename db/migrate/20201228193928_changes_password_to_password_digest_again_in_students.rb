7c9a9a2212eeeb1b50d3fdd8fb1f8e4ace7e8e79
Pedro <orsomethingelse@gmail.com>
2020-12-28 13:45:03 -0600

    adds auth

---
+++ db/migrate/20201228193928_changes_password_to_password_digest_again_in_students.rb
@@ -0,0 +1,5 @@
+class ChangesPasswordToPasswordDigestAgainInStudents < ActiveRecord::Migration[6.0]
+  def change
+    rename_column :students, :password, :password_digest
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create(email: "test2@example.com", password_digest: "password")
