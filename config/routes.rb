c82062d32cae40930ca4a347c21a0e0da92b6eba
Tziporah <tziporawerzberger@gmail.com>
2020-12-29 13:21:21 -0500

    adds education routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,11 @@ Rails.application.routes.draw do
     get "/experiences/:id" => "experiences#show"
     patch "/experiences" => "experiences#update"
     delete "/experiences/:id" => "experiences#destroy"
+
+    get "/educations" => "educations#index"
+    post "/educations" => "educations#create"
+    get "/educations/:id" => "educations#show"
+    patch "/educations" => "educations#update"
+    delete "/educations/:id" => "educations#destroy"
   end
 end
