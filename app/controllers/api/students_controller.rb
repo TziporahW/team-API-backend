52afa7ee19a0e74af99f0a5b4ddddfe89cfd9c3c
Wendy <69648973+thewendels@users.noreply.github.com>
2020-12-29 11:35:45 -0600

    adds remaining students controller actions

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -32,4 +32,38 @@ class Api::StudentsController < ApplicationController
       render json: { errors: @student.errors.full_messages }, status: :bad_request
     end
   end
+
+  def show
+    @student = Student.find(params[:id])
+    render 'show.json.jb'
+  end
+
+  def update
+    @student = Student.find(params[:id])
+
+    @student.first_name = params[:first_name] || @student.first_name
+    @student.last_name = params[:last_name] || @student.last_name
+    @student.email = params[:email] || @student.email
+    @student.phone_number = params[:phone_number] || @student.phone_number
+    @student.short_bio = params[:short_bio] || @student.short_bio
+    @student.linkedin_url = params[:linkedin_url] || @student.linkedin_url
+    @student.twitter_handle = params[:twitter_handle] || @student.twitter_handle
+    @student.personal_website_url = params[:personal_website_url] || @student.personal_website_url
+    @student.online_resume_url = params[:online_resume_url] || @student.online_resume_url
+    @student.github_url = params[:github_url] || @student.github_url
+    @student.photo_url = params[:photo_url] || @student.photo_url
+
+    if @student.save
+      render 'show.json.jb'
+    else 
+      render json: { errors: @student.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+
+    @student.destroy
+    render json: {message: "Student successfully deleted!"}
+  end
 end
