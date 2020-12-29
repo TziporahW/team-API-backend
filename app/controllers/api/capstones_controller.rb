b98a6b6c869a4c0b1eaf68c59ff697df3c5aba9a
nadia <nadiaagahozo@gmail.com>
2020-12-29 13:08:33 -0600

    updates conflicts

--- app/controllers/api/capstones_controller.rb
+++ app/controllers/api/capstones_controller.rb
@@ -27,17 +27,37 @@ class Api::CapstonesController < ApplicationController
 
   def update
     @capstone = Capstone.find_by(id: params[:id])
-    @capstone.name = params[:name] || @capstone.name
-    @capstone.description = params[:description] || @capstone.description
-    @capstone.url = params[:url] || @capstone.url
-    @capstone.screenshot_url = params[:screenshot_url] || @capstone.screenshot_url
+    if @capstone.student_id == current_student.id
+      @capstone.name = params[:name] || @capstone.name
+      @capstone.description = params[:description] || @capstone.description
+      @capstone.url = params[:url] || @capstone.url
+      @capstone.screenshot_url = params[:screenshot_url] || @capstone.screenshot_url
+      if @capstone.save
+        render "show.json.jb"
+      else
+        render json: { errors: @capstone.errors.full_messages }, status: :unprocessable_entity
+      end
+
+    end
+    
     render "show.json.jb"
+
+    
+
+
+
+
+
   end
   
   def destroy
     capstone = Capstone.find_by(id: params[:id])
-    capstone.destroy
-    render json: {message: "Capstone successfully destroyed."}
+    if capstone.student_id == current_student.id
+      capstone.destroy
+      render json: { message: "Successfully Destroyed Capstone" }
+    else
+      render json: { error: "Not your Capstone to delete!" }, status: :unauthorized
+    end
   end
 
 end
--- app/controllers/api/skills_controller.rb
+++ app/controllers/api/skills_controller.rb
@@ -25,15 +25,27 @@ class Api::SkillsController < ApplicationController
   
   def update
     @skill = skill.find_by(id: params[:id])
-    @skill.current_student_id = params[:current_student_id] || @skill.current_student_id
-    @skill.skill_name = params[:skill_name] || @skill.skill_name
-    render "show.json.jb"
+    if @skill.student_id == current_student.id
+      @skill.skill_name = params[:skill_name] || @skill.skill_name
+      if @skill.save
+        render "show.json.jb"
+      else
+        render json: { errors: @skill.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: "Not your skill to update!" }, status: :unauthorized
+    end
+
   end
   
   def destroy
     skill = Skill.find_by(id: params[:id])
-    skill.destroy
-    render json: {message: "Skill successfully destroyed."}
+    if skill.student_id == current_student.id
+      skill.destroy
+      render json: { message: "Successfully Destroyed Skill" }
+    else
+      render json: { error: "Not your Skill to delete!" }, status: :unauthorized
+    end
   end
 
 end
